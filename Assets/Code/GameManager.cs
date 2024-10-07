e1a4438271c23952f58a89577b59a024c352f501
ElMochaccino <rogermarioderex@gmail.com>
2024-10-06 18:57:56 -0600

    AvancesUI

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -46,7 +46,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             GoToInventory();
@@ -91,6 +90,8 @@ public class GameManager : MonoBehaviour
             yield return null;
         }
     }
+<<<<<<< Updated upstream
+=======
 
     public void RangeUpgrade()
     {
