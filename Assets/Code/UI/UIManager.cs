71c48f3fb7d3d854ba0fabe3154c216739b07b8f
Luis Martinez <luis.martinezfenandez@maritz.com>
2025-04-22 00:43:04 -0600

    changes

--- Assets/Code/UI/UIManager.cs
+++ Assets/Code/UI/UIManager.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
-public class UIManager : MonoBehaviour, IPointerClickHandler
+
+public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
     private QuestManager QM;
@@ -12,8 +12,8 @@ public class UIManager : MonoBehaviour, IPointerClickHandler
     [Header("UI Elements for Quests")]
     public GameObject questOfferUI;
     public TMPro.TextMeshProUGUI questOfferText;
-    public UnityEngine.UI.Button acceptButton;
-    public UnityEngine.UI.Button declineButton;
+    public Button acceptButton;
+    public Button declineButton;
 
     [Header("Quest List UI")]
     public GameObject questListUI;
@@ -61,7 +61,7 @@ public class UIManager : MonoBehaviour, IPointerClickHandler
 
 
         acceptButton.onClick.AddListener(() => {
-            Debug.Log("Hellooo");
+
             questOfferUI.SetActive(false);
             acceptButton.gameObject.SetActive(false);
             declineButton.gameObject.SetActive(false);
@@ -101,13 +101,6 @@ public class UIManager : MonoBehaviour, IPointerClickHandler
         yield return new WaitForSeconds(3f);
         questNotification.SetActive(false);
     }
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if (eventData.button == PointerEventData.InputButton.Right)
-        {
-            Debug.Log("Click derecho sobre UI detectado");
-            // Aquí podrías llamar a tu lógica de ataque, si quieres
-        }
-    }
+
 
 }
